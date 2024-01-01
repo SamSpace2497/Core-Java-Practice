4e0c960df780b46eb1c2c35094d4ae22675bb554
Shubham Patil <118250165+SamSpace2497@users.noreply.github.com>
2024-01-01 20:10:43 +0530

    Create Person.java

---
+++ com.app.school/Person.java
@@ -0,0 +1,35 @@
+package com.school;
+
+public class Person 
+{
+	private String fname;
+	private String lname;
+	
+	public Person(String fname, String lname) 
+	{
+		//System.out.println("Inside Person Constructor....");
+		this.fname = fname;
+		this.lname = lname;
+	}
+	
+	public String getFname() {
+		return fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+	@Override
+	public String toString() {
+		return "\nNAME      : "+fname+ " "+lname;
+	}
+	
+	
+
+}
