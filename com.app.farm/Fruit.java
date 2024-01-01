e2c7ec75a5534b90b7b5c60426b1df9b71c1cb66
Shubham Patil <118250165+SamSpace2497@users.noreply.github.com>
2024-01-01 20:42:16 +0530

    Create Fruit.java

---
+++ com.app.farm/Fruit.java
@@ -0,0 +1,34 @@
+package com.farm;
+
+public class Fruit 
+{
+	private String name;
+	
+	public Fruit(String name) 
+	{
+		this.name = name;
+		
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Fruit [name=" + name + "]";
+	}
+	
+	public void taste() 
+	{
+		System.out.println("TASTE");
+	}
+
+}
